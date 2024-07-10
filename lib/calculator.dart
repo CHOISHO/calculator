8b2b4653d3de63b02aab97aba5ce28442ab79108
choisho <choisho1136@gmail.com>
2024-07-11 00:34:34 +0900

    Add: Calculator 메소드, 테스트 코드 작성

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -1,5 +1,49 @@
 class Calculator {
-  static int add(a, b) {
+  late final Calculator calculator;
+
+  Calculator({Calculator? calculator}) {
+    this.calculator = calculator ?? this;
+  }
+
+  num calculate(List<num> arguments) {
+    num firtOperend = arguments[0];
+    num operation = arguments[1];
+    num secondOperend = arguments[2];
+
+    switch (operation) {
+      case 1:
+        calculator.add(firtOperend, secondOperend);
+        break;
+      case 2:
+        calculator.subtract(firtOperend, secondOperend);
+        break;
+
+      case 3:
+        calculator.multiply(firtOperend, secondOperend);
+        break;
+      case 4:
+        calculator.divide(firtOperend, secondOperend);
+        break;
+      default:
+        print("올바르지 않은 연산자 입니다.: $operation");
+    }
+
+    return 0;
+  }
+
+  num add(a, b) {
     return a + b;
+  }
+
+  num subtract(a, b) {
+    return a - b;
+  }
+
+  num multiply(a, b) {
+    return a * b;
+  }
+
+  num divide(a, b) {
+    return a / b;
   }
 }
---
+++ test/calculator_test.dart
@@ -0,0 +1,48 @@
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+import 'package:cli/calculator.dart';
+
+@GenerateNiceMocks([MockSpec<Calculator>()])
+import 'calculator_test.mocks.dart';
+
+void main() {
+  group('Calculator calculate:', () {
+    test("1 연산자가 전달되면 add 메소드가 호출된다.", () {
+      var mockCalculator = MockCalculator();
+      var calculator = Calculator(calculator: mockCalculator);
+
+      calculator.calculate([1, 1, 1]);
+
+      verify(mockCalculator.add(1, 1)).called(1);
+    });
+
+    test("2 연산자가 전달되면 subtract 메소드가 호출된다.", () {
+      var mockCalculator = MockCalculator();
+      var calculator = Calculator(calculator: mockCalculator);
+
+      calculator.calculate([1, 2, 1]);
+
+      verify(mockCalculator.subtract(1, 1)).called(1);
+    });
+
+    test("3 연산자가 전달되면 multiply 메소드가 호출된다.", () {
+      var mockCalculator = MockCalculator();
+      var calculator = Calculator(calculator: mockCalculator);
+
+      calculator.calculate([1, 3, 1]);
+
+      verify(mockCalculator.multiply(1, 1)).called(1);
+    });
+
+    test("4 연산자가 전달되면 divide 메소드가 호출된다.", () {
+      var mockCalculator = MockCalculator();
+      var calculator = Calculator(calculator: mockCalculator);
+
+      calculator.calculate([1, 4, 1]);
+
+      verify(mockCalculator.divide(1, 1)).called(1);
+    });
+  });
+}
