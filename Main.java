95d620fadc399daabf4489f134f469a35e92cd27
choisho <choisho1136@gmail.com>
2024-07-11 17:11:38 +0900

    Fix: 명령행 자바 인자

--- Main.java
+++ Main.java
@@ -2,82 +2,14 @@ import java.io.*;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        // TODO: 명령행 인자로 받도록 수정
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-        String firstOperand = getFirstOperand(reader);
-        String operation = getOperation(reader);
-        String secondOperand = getSecondOperand(reader);
-
-        String[] arguments = new String[] { firstOperand, operation, secondOperand };
+    public static void main(String[] arguments) {
+        if (arguments.length < 3) {
+            System.out.println("Usage: java Main <firstOperand> <operation> <secondOperand>");
+            return;
+        }
 
         startCalculator(arguments);
         return;
-    }
-
-    private static String getFirstOperand(BufferedReader reader) {
-        System.out.print("Enter first operand: ");
-
-        try {
-            String firstOperand = reader.readLine();
-            if (firstOperand != null) {
-                return firstOperand;
-            } else {
-                // TODO: 숫자 유효성 검사 처리
-                return "0";
-            }
-        } catch (IOException e) {
-            // TODO: 에러 처리 
-            e.printStackTrace();
-            return "0";
-        }
-    }
-    
-    private static String getSecondOperand(BufferedReader reader) {
-        System.out.print("Enter second operand: ");
-
-        try {
-            String secondOperand = reader.readLine();
-            if (secondOperand != null) {
-                return secondOperand;
-            } else {
-                // TODO: 숫자 유효성 검사 처리
-                return "0";
-            }
-        } catch (IOException e) {
-            // TODO: 에러 처리 
-            e.printStackTrace();
-            return "0";
-        }
-    }
-
-    private static String getOperation(BufferedReader reader) {
-        System.out.println("Please select an operation:");
-        System.out.println("1. Addition");
-        System.out.println("2. Subtraction");
-        System.out.println("3. Multiplication");
-        System.out.println("4. Division");
-        /* TODO:
-            5. Trigonometric functions
-            6. Calculus
-            7. logarithmic function
-         */  
-
-         try {
-            String Operation = reader.readLine();
-            if (Operation != null) {
-                return Operation;
-            } else {
-                // TODO: 옵션아닌 문자 입력시 처리 
-                // TODO: 빈 값 입력시 처리 
-                return "0";
-            }
-        } catch (IOException e) {
-            // TODO: 에러 처리 
-            e.printStackTrace();
-            return "0";
-        }
     }
 
     private static void startCalculator(String[] arguments) {
