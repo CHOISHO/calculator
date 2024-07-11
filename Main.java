700469e62a62c4475667771fdc87ea408791d896
choisho <choisho1136@gmail.com>
2024-07-11 14:52:39 +0900

    Add: Java Main startCalculator 작성

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,11 +10,9 @@ public class Main {
         String operation = getOperation(reader);
         String secondOperand = getSecondOperand(reader);
 
-        System.err.println(firstOperand);
-        System.err.println(operation);
-        System.err.println(secondOperand);
+        String[] arguments = new String[] { firstOperand, operation, secondOperand };
 
-        // TODO: dart 계산기 argument 로 전달 
+        startCalculator(arguments);
         return;
     }
 
@@ -78,6 +77,36 @@ public class Main {
             // TODO: 에러 처리 
             e.printStackTrace();
             return "0";
+        }
+    }
+
+    private static void startCalculator(String[] arguments) {
+        try {
+            // Dart 프로세스를 실행
+            ProcessBuilder processBuilder = new ProcessBuilder("dart", "bin/main.dart");
+            
+            processBuilder.command().addAll(List.of(arguments));
+
+            processBuilder.redirectErrorStream(true);
+
+            // 프로세스 시작
+            Process process = processBuilder.start();
+
+            // 프로세스의 출력 스트림을 읽음
+            BufferedReader processReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String line;
+            
+            while ((line = processReader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            // 프로세스 종료를 기다림
+            int exitCode = process.waitFor();
+            System.out.println("Exited with code: " + exitCode);
+          
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
